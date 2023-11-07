066f9b0d56906237252ae22ae9895523c3ef0624
memo <alexis45645@gmail.com>
2023-11-06 22:22:59 -0500

    Part02Lab04

--- LAB04/PART02/LAB-04-Part-2-Alejandro/Assets/Scripts/PlayerMovementWASD.cs
+++ LAB04/PART02/LAB-04-Part-2-Alejandro/Assets/Scripts/PlayerMovementWASD.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class PlayerMovementWASD : MonoBehaviour
 {
     private CharacterController controller;
-    private float speed = 5f;
+    private float speed = 3f;
 
     private const float turnSmoothTime = .2f;
     private float turnSmoothVelocity;
 
-    private float jumpHeight = 4f;
+    private float jumpHeight = 1.5f;
 
     private Animator anim;
 
@@ -18,43 +18,68 @@ public class PlayerMovementWASD : MonoBehaviour
 
     private Vector3 moveVelocity;
 
+    private const float groundDistance = .2f;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private Transform groundCheck;
+    private bool isGrounded => Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
     private void Awake()
     {
         controller = GetComponent<CharacterController>();
         cam = Camera.main.transform;
         anim = GetComponent<Animator>();
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
     private void Update()
     {
+        if (isGrounded && moveVelocity.y < 0)
+        {
+            moveVelocity.y = -2f;
+        }
+
+
         movePlayer();
+
     }
     private void movePlayer()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 _dir= new Vector3(horizontal, 0f, vertical).normalized*speed;
-        moveVelocity = new Vector3(_dir.x, moveVelocity.y, _dir.z);
-        if (controller.isGrounded)
+        Vector3 _dir = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (isGrounded)
         {
-            
-            if (Input.GetButtonDown("Jump"))
+            if (_dir == Vector3.zero)
             {
-                moveVelocity.y = jumpHeight;
+                idle();
             }
         }
-        else
+
+        if (_dir.magnitude >= 0.1f)
         {
-            moveVelocity.y += Physics.gravity.y * Time.deltaTime;
+            float targetAngle = Mathf.Atan2(_dir.x, _dir.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            Vector3 movedir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            if (isGrounded && movedir != Vector3.zero)
+            {
+                walk();
+            }
+
+            controller.Move(speed * Time.deltaTime * movedir.normalized);
+
         }
 
-        float targetAngle = Mathf.Atan2(moveVelocity.x, moveVelocity.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        if (Input.GetButtonDown("Jump")&&isGrounded)
+        {
+            moveVelocity.y=Mathf.Sqrt(jumpHeight*-2*Physics.gravity.y);
+            triggerAnim("Jump");
+        }
 
-        Vector3 movedir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-
-        controller.Move(moveVelocity*Time.deltaTime);
-      
+        moveVelocity.y += Physics.gravity.y * Time.deltaTime;
+        controller.Move(moveVelocity * Time.deltaTime);
     }
 
     private void walk()
@@ -75,5 +100,9 @@ public class PlayerMovementWASD : MonoBehaviour
     private void animate(float _value, float _dampTime = .1f)
     {
         anim.SetFloat("Speed", _value, _dampTime, Time.deltaTime);
+    }
+    private void triggerAnim(string _trigger)
+    {
+        anim.SetTrigger(_trigger);
     }
 }
